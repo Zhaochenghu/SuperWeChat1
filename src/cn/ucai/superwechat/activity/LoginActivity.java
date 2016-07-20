30e408eece0f8c08adbc7e4b244937e2401dc9d0
Zhaochenghu <3469142621@qq.com>
2016-07-20 18:01:56 +0800

    登录成功后保存用户信息到全局变量

--- src/cn/ucai/superwechat/activity/LoginActivity.java
+++ src/cn/ucai/superwechat/activity/LoginActivity.java
@@ -176,18 +176,22 @@ public class LoginActivity extends BaseActivity {
 	}
 
 	private void loginAppSever() {
-		final OkHttpUtils2<Result> utils = new OkHttpUtils2<Result>();
+		final OkHttpUtils2<String> utils = new OkHttpUtils2<String>();
 		utils.setRequestUrl(I.REQUEST_LOGIN)
 				.addParam(I.User.USER_NAME,currentUsername)
 				.addParam(I.User.PASSWORD,currentPassword)
-				.targetClass(Result.class)
-				.execute(new OkHttpUtils2.OnCompleteListener<Result>() {
+				.targetClass(String.class)
+				.execute(new OkHttpUtils2.OnCompleteListener<String>() {
 					@Override
-					public void onSuccess(Result result) {
+					public void onSuccess(String s) {
+						Log.e(TAG,"s="+s);
+						Result result = Utils.getListResultFromJson(s, UserAvatar.class);
 						if (result != null && result.isRetMsg()) {
 							UserAvatar user = (UserAvatar) result.getRetData();
-							saveUserToDB(user);
-							loginSuccess();
+							if (user != null) {
+								saveUserToDB(user);
+								loginSuccess(user);
+							}
 						} else {
 							pd.dismiss();
 							Toast.makeText(getApplicationContext(),R.string.Login_failed
@@ -206,17 +210,18 @@ public class LoginActivity extends BaseActivity {
 	}
 
 	private void saveUserToDB(UserAvatar user) {
-		if (user != null) {
-			//存入db
-			UserDao dao = new UserDao(LoginActivity.this);
-			dao.saveUderAvatar(user);
-		}
+		//存入db
+		UserDao dao = new UserDao(LoginActivity.this);
+		dao.saveUderAvatar(user);
+
 	}
 
-	private void loginSuccess() {
+	private void loginSuccess(UserAvatar user) {
 		// 登陆成功，保存用户名密码
 		SuperWeChatApplication.getInstance().setUserName(currentUsername);
 		SuperWeChatApplication.getInstance().setPassword(currentPassword);
+		SuperWeChatApplication.getInstance().setUser(user);
+		SuperWeChatApplication.currentUserNick = user.getMUserNick();
 
 		try {
 			// ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
