abb21d156ebf6464fe03e372333d1a5ec8fdb15b
Zhaochenghu <3469142621@qq.com>
2016-07-21 20:51:23 +0800

    联系人界面的搜索功能

--- src/cn/ucai/superwechat/adapter/ContactAdapter.java
+++ src/cn/ucai/superwechat/adapter/ContactAdapter.java
@@ -208,8 +208,11 @@ public class ContactAdapter extends ArrayAdapter<User>  implements SectionIndexe
 					final User user = mOriginalList.get(i);
 					String username = user.getUsername();
 					
-					if(username.startsWith(prefixString)){
-						newValues.add(user);
+					if(username.contains(prefixString)){
+						if (!username.equals(Constant.GROUP_USERNAME) &&
+								!username.equals(Constant.NEW_FRIENDS_USERNAME)) {
+							newValues.add(user);
+						}
 					}
 					else{
 						 final String[] words = username.split(" ");
