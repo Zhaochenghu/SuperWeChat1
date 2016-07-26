e0ea46de1494585ba8a413db8c82910552374cf2
Zhaochenghu <3469142621@qq.com>
2016-07-26 12:50:21 +0800

    统一当前登录用户的头像显示

--- src/cn/ucai/superwechat/activity/UserProfileActivity.java
+++ src/cn/ucai/superwechat/activity/UserProfileActivity.java
@@ -287,11 +287,11 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
             Log.e(TAG, "upload avatar to app server...");
 			dialog = ProgressDialog.show(this, getString(R.string.dl_update_nick), getString(R.string.dl_waiting));
 			dialog.show();
-            uploadUserAvatar();
+            uploadUserAvatar(data);
         }
     }
 
-    private void uploadUserAvatar() {
+    private void uploadUserAvatar(final Intent data) {
         File file = new File(OnSetAvatarListener.getAvatarPath(UserProfileActivity.this, I.AVATAR_TYPE_USER_PATH),
                 avatarName + I.AVATAR_SUFFIX_JPG);
         String username = SuperWeChatApplication.getInstance().getUserName();
@@ -305,9 +305,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
                     public void onSuccess(Result result) {
                         Log.e(TAG, "result=" + result);
 						if (result.isRetMsg()) {
-							dialog.dismiss();
-							Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatephoto_success),
-									Toast.LENGTH_SHORT).show();
+							setPicToView(data);
 						} else {
 							Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatephoto_fail),
 									Toast.LENGTH_SHORT).show();
@@ -354,7 +352,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 	}
 	
 	private void uploadUserAvatar(final byte[] data) {
-		dialog = ProgressDialog.show(this, getString(R.string.dl_update_photo), getString(R.string.dl_waiting));
+		//dialog = ProgressDialog.show(this, getString(R.string.dl_update_photo), getString(R.string.dl_waiting));
 		new Thread(new Runnable() {
 
 			@Override
@@ -365,6 +363,7 @@ public class UserProfileActivity extends BaseActivity implements OnClickListener
 					public void run() {
 						dialog.dismiss();
 						if (avatarUrl != null) {
+							Log.e(TAG, "avatarUrl=" + avatarUrl);
 							Toast.makeText(UserProfileActivity.this, getString(R.string.toast_updatephoto_success),
 									Toast.LENGTH_SHORT).show();
 						} else {
