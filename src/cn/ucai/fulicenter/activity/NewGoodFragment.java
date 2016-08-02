b8172f3b8ca886a29d8bf5f3d5921b247f67b8ad
Zhaochenghu <3469142621@qq.com>
2016-08-02 16:34:11 +0800

    新品fragment的上拉刷新操作

--- src/cn/ucai/fulicenter/activity/NewGoodFragment.java
+++ src/cn/ucai/fulicenter/activity/NewGoodFragment.java
@@ -33,7 +33,7 @@ public class NewGoodFragment extends Fragment{
     GridLayoutManager mGridLayoutManager;
     GoodAdapter mAdapter;
     List<NewGoodBean> mGoodList;
-    int pageId = 1;
+    int pageId = 0;
 
     TextView tvHint;
     @Nullable
@@ -51,6 +51,35 @@ public class NewGoodFragment extends Fragment{
 
     private void setListener() {
         setPullDownRefreshListener();
+        setPullUpRefreshListener();
+    }
+
+    private void setPullUpRefreshListener() {
+        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastItemPosition;
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int a = RecyclerView.SCROLL_STATE_DRAGGING;//1
+                int b = RecyclerView.SCROLL_STATE_IDLE;//0
+                int c = RecyclerView.SCROLL_STATE_SETTLING;//2
+                Log.e(TAG, "newState=" + newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastItemPosition == mAdapter.getItemCount() - 1) {
+                    pageId += I.PAGE_SIZE_DEFAULT; 
+                    initData();
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int f = mGridLayoutManager.findFirstVisibleItemPosition();
+                int l = mGridLayoutManager.findLastVisibleItemPosition();
+                Log.e(TAG, "f=" + f + ",l=" + l);
+                lastItemPosition = mGridLayoutManager.findLastVisibleItemPosition();
+            }
+        });
     }
 
     private void setPullDownRefreshListener() {
