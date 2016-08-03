bf6591018788b566bb98f6d6cedbab1027d0fb00
Zhaochenghu <3469142621@qq.com>
2016-08-03 16:01:00 +0800

    商品详情页面的图片轮播及返回按钮

--- src/cn/ucai/fulicenter/activity/GoodDetailActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailActivity.java
@@ -11,9 +11,11 @@ import android.widget.Toast;
 import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.AlbumsBean;
 import cn.ucai.fulicenter.bean.GoodDetailsBean;
 import cn.ucai.fulicenter.data.OkHttpUtils2;
 import cn.ucai.fulicenter.utils.ImageUtils;
+import cn.ucai.fulicenter.view.DisplayUtils;
 import cn.ucai.fulicenter.view.FlowIndicator;
 import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
@@ -35,7 +37,7 @@ public class GoodDetailActivity extends BaseActivity {
     SlideAutoLoopView mSlideAutoLoopView;
     FlowIndicator mFlowIndicator;
     WebView wvGoodBrief;
-
+    //GoodDetailsBean mGoodDetail;
     int mGoodId;
     @Override
     protected void onCreate(Bundle arg0) {
@@ -75,6 +77,28 @@ public class GoodDetailActivity extends BaseActivity {
         tvGoodName.setText(details.getGoodsName());
         tvGoodPriceCurrent.setText(details.getCurrencyPrice());
         tvGoodPriceShop.setText(details.getShopPrice());
+        mSlideAutoLoopView.startPlayLoop(mFlowIndicator, getAlbumImageUrl(details),
+                getAlbumImageSize(details));
+        wvGoodBrief.loadDataWithBaseURL(null, details.getGoodsBrief(), D.TEXT_HTML, D.UTF_8, null);
+    }
+
+    private String[] getAlbumImageUrl(GoodDetailsBean details) {
+        String[] albumImageUrl = new String[]{};
+        if (details.getProperties() != null && details.getProperties().length > 0) {
+            AlbumsBean[] albums = details.getProperties()[0].getAlbums();
+            albumImageUrl=new String[albums.length];
+            for (int i=0;i<albumImageUrl.length;i++) {
+                albumImageUrl[i] = albums[i].getImgUrl();
+            }
+        }
+        return albumImageUrl;
+    }
+
+    private int getAlbumImageSize(GoodDetailsBean details) {
+        if (details.getProperties() != null && details.getProperties().length > 0) {
+            return details.getProperties()[0].getAlbums().length;
+        }
+        return 0;
     }
 
     private void getGoodDetailsByGoodId(OkHttpUtils2.OnCompleteListener<GoodDetailsBean> listener){
@@ -86,6 +110,7 @@ public class GoodDetailActivity extends BaseActivity {
     }
 
     private void initView() {
+        DisplayUtils.initBack(mContext);
         ivShare = (ImageView) findViewById(R.id.ivShare);
         ivCollect = (ImageView) findViewById(R.id.ivCollect);
         ivCart = (ImageView) findViewById(R.id.ivAddCart);
