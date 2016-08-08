202bdae1a851329f3311031f3d52f3bb65656d2c
Zhaochenghu <3469142621@qq.com>
2016-08-08 15:14:31 +0800

    添加登录逻辑

--- src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
+++ src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -9,6 +10,7 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 import cn.ucai.fulicenter.BoutiqueFragment;
+import cn.ucai.fulicenter.DemoHXSDKHelper;
 import cn.ucai.fulicenter.R;
 
 /**
@@ -34,7 +36,7 @@ public class FuliCenterMainActivity extends BaseActivity{
     private Fragment[] fragments;
 
     private int currentTabIndex;
-
+    public static final int ACTION_LOGIN = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,11 +96,19 @@ public class FuliCenterMainActivity extends BaseActivity{
                 index = 3;
                 break;
             case R.id.layout_personal:
-                index = 4;
+                if (DemoHXSDKHelper.getInstance().isLogined()) {
+
+                    index = 4;
+                } else {
+                    gotoLogin();
+                }
                 break;
         }
         Log.e(TAG, "index=" + index + ",currentIndex=" + currentIndex);
+        setFragment();
+    }
 
+    private void setFragment() {
         if (currentTabIndex != index) {
             FragmentTransaction trx = getSupportFragmentManager().beginTransaction();
             trx.hide(fragments[currentTabIndex]);
@@ -111,6 +121,10 @@ public class FuliCenterMainActivity extends BaseActivity{
         }
     }
 
+    private void gotoLogin() {
+        startActivityForResult(new Intent(this,LoginActivity.class),ACTION_LOGIN);
+    }
+
     private void setRadioButtonStatus(int index) {
        for (int i=0;i<mrbTabs.length;i++) {
            if (index == i) {
@@ -121,4 +135,29 @@ public class FuliCenterMainActivity extends BaseActivity{
        }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTION_LOGIN) {
+            if (DemoHXSDKHelper.getInstance().isLogined()) {
+
+            } else {
+                setRadioButtonStatus(currentIndex);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (DemoHXSDKHelper.getInstance().isLogined()) {
+
+        } else {
+            index = currentIndex;
+            if (index == 4) {
+                index = 0;
+            }
+            setFragment();
+        }
+    }
 }
