8e885b8606ef031562246a9562c9c08fdcf2b90b
Zhaochenghu <3469142621@qq.com>
2016-08-11 16:25:28 +0800

    导入壹收款SDK

--- src/cn/ucai/fulicenter/activity/BuyActivity.java
+++ src/cn/ucai/fulicenter/activity/BuyActivity.java
@@ -1,6 +1,8 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -16,18 +18,32 @@ public class BuyActivity extends BaseActivity {
     EditText edOrderPhone;
     Spinner spinProvince;
     EditText edOrderStreet;
+    Button btnBuy;
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
+        mContext = this;
         setContentView(R.layout.activity_buy);
         initView();
+        setListener();
+    }
+
+    private void setListener() {
+        btnBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void initView() {
+        //DisplayUtils.initBackWithTitle(mContext,"填写收货地址");
         DisplayUtils.initBackWithTitle(mContext,"填写收货地址");
         edOrderName = (EditText) findViewById(R.id.et_order_name);
         edOrderPhone = (EditText) findViewById(R.id.et_order_phone);
         edOrderStreet = (EditText) findViewById(R.id.et_order_street);
         spinProvince = (Spinner) findViewById(R.id.spin_order_province);
+        btnBuy = (Button) findViewById(R.id.btn_buy);
     }
 }
